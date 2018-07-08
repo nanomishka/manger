f545ad842dcbf54f72cdbebdb49365f568ea28df
nanomishka <mihail777str@yandex.ru>
2018-07-08 21:39:44 +0300

    startapp manger

--- application/settings.py
+++ application/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'manger.urls'
+ROOT_URLCONF = 'application.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'manger.wsgi.application'
+WSGI_APPLICATION = 'application.wsgi.application'
 
 
 # Database
